9bc6d234ff226b96577a732ca469a6b0ca618807
Sterling Hamilton <sterling.hamilton@gmail.com>
2014-07-30 08:57:42 -0700

    Update main script to work again. Last updates to refactor were not tested enough. This is better. Also increased the interval because latency can cause light flickering if it alternates too much. Added logging information useful for troubleshooting results.

--- internet-status.rb
+++ internet-status.rb
@@ -3,19 +3,19 @@ require 'rest_client'
 require 'net/http'
 require 'net/ping/http'
 
-def every_5_seconds
+def every_10_seconds
 		last = Time.now
 		while true
 				yield
 				now = Time.now
-				_next = [last + 5,now].max
+				_next = [last + 10,now].max
 				sleep (_next-now)
 				last = _next
 		end
 end
 
 def check_internet
-	lightbulb_id = 3
+	lightbulb_id = '3'
 	primary_internet = '68.190.186.130'
 	backup_internet = '66.224.72.122'
 	hub_ip = '192.168.16.155'
@@ -24,29 +24,34 @@ def check_internet
 	default = 'http://google.com'
 
 	on = false
+	yellow = 18373 # Back up internet.
+	green = 25654 # Primary internet and speeds are OK
+	red = 65527 # Slow speeds -- regardless of ISP
 	hue = red
-	yellow = 20000 # Back up internet.
-	green = 25500 # Primary internet and speeds are OK
-	red = 0 # Slow speeds -- regardless of ISP
 
 	request = Net::Ping::HTTP.new(default)
+
 	if request.ping
+		puts "#{Time.now}	#{request.duration}"
+		on = true
 		if request.duration < 1
 			public_ip = RestClient.get 'http://whatismyip.akamai.com'
-			on = true
 			if public_ip == primary_internet
 				hue = green
 			else
+				puts "#{Time.now}	BACKUP INTERNET"
 				hue = yellow
 			end
 		else
 			hue = red
 		end
-			on = false
 	else
-
+		puts "#{Time.now}	INTERNET DOWN"
+		on = false
 	end
-	RestClient.put api_path + '/lights/' + lightbulb_id + '/state', {:on => on, :hue => hue, :bri => 200}.to_json, :content_type => :json, :accept => :json
+
+	RestClient.put api_path + '/lights/' + lightbulb_id + '/state', {:on => on, :hue => hue, :bri => 80, :sat => 255}.to_json, :content_type => :json, :accept => :json
+
 end
 
-every_5_seconds { check_internet }
+every_10_seconds { check_internet }
